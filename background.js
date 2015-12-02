b2e89bf37e95de7ff53a36a2c58d5433ab898766
Stéphane Adam Garnier <seedsodesign@gmail.com>
2015-12-02 17:32:11 +0100

    Create background.js

---
+++ background.js
@@ -0,0 +1,16 @@
+/**
+ * Listens for the app launching then creates the window
+ *
+ * @see http://developer.chrome.com/apps/app.runtime.html
+ * @see http://developer.chrome.com/apps/app.window.html
+ */
+chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.app.window.create('index.html', {
+    //id: "camCaptureID",
+    id: "tefCamID",
+    innerBounds: {
+      width: 700,
+      height: 600
+    }
+  });
+});
